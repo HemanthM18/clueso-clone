71ab9037762b4712c717ea37abab120b2fb426a9
HemanthM18 <141708170+HemanthM18@users.noreply.github.com>
2025-12-29 16:43:25 +0530

    Implement email/password authentication with JWT

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,14 +3,16 @@ const cors = require('cors');
 require('dotenv').config();
 
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// DB Connection
 connectDB();
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Clueso Backend API running');
